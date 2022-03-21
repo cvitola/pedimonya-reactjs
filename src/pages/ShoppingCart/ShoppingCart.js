7e5019c762dc78ca146d4bfe19c2075571f0c1b4
Cristian <isrunaway@gmail.com>
2022-03-21 12:59:33 -0300

    detalles

--- src/pages/ShoppingCart/ShoppingCart.js
+++ src/pages/ShoppingCart/ShoppingCart.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from "react-redux";
-import { ContainerFirst, H1, Container, H2, Button, KeyPad } from '../../components/BasicStyles/BasicStyles';
+import { ContainerFirst, H1, Container, H2, Button, KeyPad, P } from '../../components/BasicStyles/BasicStyles';
 import  CardCart from '../../components/CardCart/CardCart';
 import { DeleteCartAction } from '../../redux/pokemones';
 
@@ -9,6 +9,7 @@ const ShoppingCart = () => {
   //const myCart = useSelector(store => store.pokes.shoppingPokes)
   const dispatch = useDispatch();
   const { shoppingPokes } = useSelector( store => store.pokes )
+  const [infoBuy, setInfoBuy] = useState("");
   const amountTotal = () => {
 
     return shoppingPokes?.reduce((acum, current) => acum + current.price, 0);
@@ -26,15 +27,29 @@ const ShoppingCart = () => {
     
   }
 
+  const handleBuyCart = () => {
+    //a modo de emular la compra del carrito
+    if(shoppingPokes.length !== 0){
+      const newArray = [];
+      dispatch(DeleteCartAction(newArray))
+      let random = Math.round(Math.random() * 1000);
+      setInfoBuy(`Adquiriste el carro ${random}`);
+    }else{
+      setInfoBuy("");
+    }
+    
+  }
+
   useEffect( () => { paintCards(shoppingPokes)},[shoppingPokes])
 
   return (
       <ContainerFirst>
         <H1>MI PEDIDO</H1>
         <H2>TOTAL: ${amountTotal()}</H2>
+        <P>{infoBuy}</P>
         <KeyPad>
-          <Button>COMPRAR</Button>
-          <Button onClick={handleEmptyCart}>VACIAR</Button>   
+          <Button onClick={handleBuyCart}>COMPRAR</Button>
+          <Button onClick={handleEmptyCart} warning={true}>VACIAR</Button>   
         </KeyPad>
 
 
